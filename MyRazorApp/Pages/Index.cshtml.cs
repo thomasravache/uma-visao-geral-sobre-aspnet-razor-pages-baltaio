6036b2527c2c85e292a3c41712f7b66e19172cab
Thomas Ravache <thomasravache31@gmail.com>
2023-05-24 09:00:06 -0300

    feat: Criando um Razor Page

---
+++ MyRazorApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyRazorApp
+{
+  public class Index : PageModel
+  {
+    private readonly ILogger<Index> _logger;
+
+    public Index(ILogger<Index> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnGet() // Sempre que fizer um get na pagina ele passa nesse método - obter
+    {
+    }
+
+    public void OnPost() // Sempre que fizer um post na pagina ele passa aqui - enviar
+    {
+
+    }
+  }
+}
