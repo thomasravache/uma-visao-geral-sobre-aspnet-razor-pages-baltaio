44c7620baa1cb30ebd9d1c51638cc458caa49438
Thomas Ravache <thomasravache31@gmail.com>
2023-05-24 08:44:32 -0300

    feat: Adicionando suporte a Razor Pages

--- MyRazorApp/Program.cs
+++ MyRazorApp/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages(); // Adicionando suporte para páginas razor
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection(); // Usar HTTPS
+app.UseStaticFiles(); // Arquivs estaticos (css, js, imagens, jsons, etc..) precisa ter a pasta wwwroot
+
+// Auxilia no mapeamento das paginas e criacao de urls personalizadas
+app.UseRouting();
+app.MapRazorPages();
 
 app.Run();
