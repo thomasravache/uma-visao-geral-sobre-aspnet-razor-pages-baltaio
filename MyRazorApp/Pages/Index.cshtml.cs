02c651bb42059167352b49468c9e1623104868b3
Thomas Ravache <thomasravache31@gmail.com>
2023-05-25 07:18:42 -0300

    feat: C# e HTML juntos

--- MyRazorApp/Pages/Index.cshtml.cs
+++ MyRazorApp/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace MyRazorApp
+namespace MyRazorApp.Pages
 {
   public class Index : PageModel
   {
